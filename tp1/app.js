8f56ede4a619f6b364498b2f9a997e6d12d84411
Akvir03 <nathan.devienne.pro@gmail.com>
2023-02-03 10:45:21 +0100

    Fin TP1

--- tp1/app.js
+++ tp1/app.js
@@ -3,6 +3,37 @@ const app = express()
 const port = 3000
 const fs = require('fs')
 const stream = require('stream')
+
+/*Q9*/
+var uptime = Math.round(process.uptime());
+var list = {
+  status: "healthy",
+  requestsCount: {
+    "/": 0,
+    "/welcome": 0,
+    "/secret": 0,
+    "/redirectMe": 0,
+    "/error": 0,
+    "/img": 0,
+    "/somme": 0,
+    "/users": 0,
+    "/metrics":0,
+  },
+  uptime: uptime,
+};
+
+app.use("/", function (req, res, next) {
+  var requests = req.url.split('/');
+  var request = `/${requests[1]}`;
+  var ifsomme = request.split('?');
+
+  if (ifsomme[0]=="somme"){
+    request = `/${ifsomme[0]}`;
+  }
+  list['requestsCount'][request]++;
+  next();
+})
+
 /*Q8*/
 app.use("/", function (req, res, next) {
   var date = new Date().toISOString();
@@ -10,6 +41,8 @@ app.use("/", function (req, res, next) {
   console.log(`[${date}] : ${url}`);
   next();
 });
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -42,6 +75,10 @@ app.get('/users/:name', (req, res) => {
 /*Q7*/
 app.get('/somme', (req, res) => {
   res.send("Somme = " + (parseInt(req.query.a) + parseInt(req.query.b)))
+})
+/*Q9*/
+app.get('/metrics', (req, res) => {
+  res.json(list)
 })
 
 app.listen(port, () => {
