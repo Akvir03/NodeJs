f79d37217eb2bf9e49437b4fe78e07c0081c561a
Akvir03 <nathan.devienne.pro@gmail.com>
2023-03-08 15:33:33 +0100

    Push

--- tp2/src/controllers/users.js
+++ tp2/src/controllers/users.js
@@ -1,10 +1,61 @@
 const { findOne, find, insertOne, updateOne} = require("../services/db/crud");
-
+const axios = require("axios").default;
+const { getMovieByTitle} = require("../repositories/omdbapi.js");
 
 async function findUser(req, res, next) {
   try {
     const result = await findOne("User", {"username": req});
     return res.send(result);
+  } catch (e) {
+    console.log(e);
+    return next(e);
+  }
+}
+
+async function insertfilminWatch(req,res,next){
+  try {
+    const verif = await findOne("User", {username: req.body.username})
+    console.log(verif)
+    if (verif) {
+      const verifwatch = await findOne("Watchlist",{nom: req.body.nom})
+      if(verifwatch){
+        delete req.body.username
+          const veriffilm = await findOne("Registre",{Title: req.body.Title})
+          if(veriffilm){
+            let filmapush = {
+              "Title":req.body.Title,
+              "statu":req.body.statu,
+            }
+          verifwatch.filmlist.push(filmapush)
+          const result = await updateOne("Watchlist", {"filmlist":verifwatch.filmlist}, {$set : {filmlist:verifwatch.filmlist}})
+          return res.send(result);
+        }
+          else{
+          const film = await getMovieByTitle(req.body.Title)
+          let filmform = {
+            "Title":film.Title,
+            "statu":req.body.statu,
+          }
+          const ajoutregistr = await insertOne("Registre", filmform);
+          verifwatch.filmlist.push(filmform);
+          const result = await updateOne("Watchlist", {"filmlist":verifwatch.filmlist}, {$set : {filmlist:verifwatch.filmlist}})
+          return res.send(result);
+        }
+        
+        
+      }
+      else{
+        return res.status(409).send({Error: `Error, la watchlist ${req.body.nom} n'existe pas`});
+        
+      }
+      
+    }
+    else{
+      return res.status(404).send({Error: `Error, l'utilisateur ${verif.username} n'existe pas`});
+      
+    }
+      
+    
   } catch (e) {
     console.log(e);
     return next(e);
@@ -21,6 +72,14 @@ async function insertWatch(req,res,next){
         return res.status(409).send({Error: `Error, la watchlist ${req.body.nom} existe déja`});
       }
       else{
+        req.body.user_id=verif._id
+        delete req.body.username
+        if(req.body.filmlist==null){
+          req.body.filmlist=[]
+        }
+        if(req.body.serielist==null){
+          req.body.serielist=[]
+        }
         const result = await insertOne("Watchlist", req.body);
         return res.send(result);
       }
@@ -37,6 +96,31 @@ async function insertWatch(req,res,next){
     return next(e);
   }
 }
+async function insertfilminregistre(req,res,next){
+    try {
+        const veriffilm = await findOne("Registre",{Title: req.body.Title})
+        if(veriffilm){
+          return res.status(409).send({Error: `Error, le film ${req.body.Title} existe déja`});
+        }
+        else{
+          const film = await getMovieByTitle(req.body.Title)
+          let filmform = {
+            "Title":film.Title,
+            "Year":film.Year,
+            "Director":film.Director,
+            "Poster":film.Poster
+          }
+          const result = await insertOne("Registre", filmform);
+          return res.send(result);
+        }
+        
+      }          
+      catch (e) {
+      console.log(e);
+      return next(e);
+    }
+  }
+
 
 async function findUsers(req, res, next) {
   try {
@@ -79,5 +163,5 @@ async function updateUser(req, res, next) {
 }
 
 module.exports = {
-  findUser, findUsers, insertUser, updateUser, insertWatch
+  findUser, findUsers, insertUser, updateUser, insertWatch, insertfilminWatch, insertfilminregistre
 };
--- tp2/src/routes/users.js
+++ tp2/src/routes/users.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { findUser,insertUser,updateUser,findUsers,insertWatch} = require("../controllers/users");
+const { findUser,insertUser,updateUser,findUsers,insertWatch, insertfilminWatch, insertfilminregistre} = require("../controllers/users");
 
 router.get("/findone", findUser);
 router.post("/create", insertUser);
 router.get("/find", findUsers);
 router.post("/update",updateUser);
 router.post("/createwatchlist", insertWatch);
+router.post("/insertfilminwatchlist",insertfilminWatch);
+router.post("/insertfilm",insertfilminregistre)
 
 module.exports = router;
